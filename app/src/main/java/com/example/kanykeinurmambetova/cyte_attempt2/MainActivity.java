b053229d45668c3516e66b02410151d9ae5810b7
kanykeinurmambetova <kanykei2610@gmail.com>
2019-03-20 15:53:58 -0600

    First slide UI elements

--- app/src/main/java/com/example/kanykeinurmambetova/cyte_attempt2/MainActivity.java
+++ app/src/main/java/com/example/kanykeinurmambetova/cyte_attempt2/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.kanykeinurmambetova.cyte_attempt2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declaring our buttons
+    TextView welcome;
+    Button employee;
+    Button employer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
